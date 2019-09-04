af1e69f369a587dde7555485ab662b828bccc5e1
Eli Kittross-Schnell <eliks92@gmail.com>
2019-09-04 22:04:18 +0000

    re-commit because of updated files

--- hello.js
+++ hello.js
@@ -1,5 +1,16 @@
-const sayHello  = function () {
+/*const sayHello  = function () {
   console.log("Hello, world");
 }
 
-sayHello();
+
+sayHello();*/
+
+
+const sayHello  = function (name) {
+  console.log("Hello, " + name);
+}
+
+
+sayHello("Caliban");
+sayHello("Miranda");
+sayHello("Ferdinand");
